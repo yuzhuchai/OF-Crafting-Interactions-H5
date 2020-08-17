c497186655839e7223ca04121b8b043c4417fe58
yuzhu chai <yuzhupoemhead@gmail.com>
2020-08-17 16:40:51 +0800

    debug: wrapping jquery inside .ready()

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 console.log('connected')
 
-$('.answers').click(function(){
-	console.log("clicked")
-	$('.answers').css({'background-color':'white', 'color':'black'})
-	$(this).css({'background-color':'black', 'color':'white'})
-})
+// bug, jquery not working on the site, but works when written in console.
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.answers').click(function(){
+		console.log("clicked")
+		$('.answers').css({'background-color':'white', 'color':'black'})
+		$(this).css({'background-color':'black', 'color':'white'})
+	})
+});
+
+
 
