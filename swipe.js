c641a9036ba99701aa18a83718c03ef400fc843c
新媒体01 <designer01@shejishi01deiMac.local>
2020-08-20 00:40:33 +0800

    fix animation bug

--- swipe.js
+++ swipe.js
@@ -38,7 +38,7 @@ $( document ).ready(function() {
 			handleGesure('homePageContainer','teacherPageContainer','firstQuestionContainer')
 			// this is displaying the page on the right, with color blue to red 
 			if(swipedLeft){
-				$('body').css({'background':'linear-gradient(270deg, #e8005f, #1818ed)'})
+				$('body').css({'background':'linear-gradient(270deg, #e8005f, #1818ed)','background-size':'400% 400%'})
 				swipedLeft = false
 			}
 		} else if(currentPageDisplay == 'teacherPageContainer'){
@@ -47,27 +47,27 @@ $( document ).ready(function() {
 			//yhid id displayin the page on the right, with color no change 
 			//thi is dispayign thr page on the left, with color, blue to red 
 		 	if (swipedLeft){
-				$('body').css({'background':'linear-gradient(270deg, #e8005f, #1818ed)'})
+				$('body').css({'background':'linear-gradient(270deg, #e8005f, #1818ed)','background-size':'400% 400%'})
 				swipedLeft = false
 			}
 		} else if(currentPageDisplay == 'firstQuestionContainer'){
 			handleGesure('firstQuestionContainer','secondQuestionContainer','teacherPageContainer')
 			//htsi is dsiplaying the page on the rihgt 
 			if(swipedLeft){
-				$('body').css({'background':'linear-gradient(270deg, #1818ed, #12002b)'})
+				$('body').css({'background':'linear-gradient(270deg, #1818ed, #12002b)','background-size':'400% 400%'})
 				swipedLeft = false
 			} else if(swipedRight){
-				$('body').css({'background':'linear-gradient(270deg, #14e236, #e8005f)'})
+				$('body').css({'background':'linear-gradient(270deg, #14e236, #e8005f)','background-size':'400% 400%'})
 				swipedRight = false
 			}
 		} else if (currentPageDisplay == 'secondQuestionContainer'){
 			// $('body').css('background-color','#e8005f');
 			handleGesure('secondQuestionContainer','thirdQuestionContainer','teacherPageContainer')
 			if(swipedLeft){
-				$('body').css({'background':'linear-gradient(270deg, #1818ed, #12002b)'})
+				$('body').css({'background':'linear-gradient(270deg, #1818ed, #12002b)','background-size':'400% 400%'})
 				swipedLeft = false
 			} else if(swipedRight){
-				$('body').css({'background':'linear-gradient(270deg, #14e236, #f4ee19)'})
+				$('body').css({'background':'linear-gradient(270deg, #14e236, #f4ee19)','background-size':'400% 400%'})
 				swipedRight = false
 			}
 		} else if (currentPageDisplay == 'thirdQuestionContainer'){
@@ -75,10 +75,10 @@ $( document ).ready(function() {
 			console.log('change bg color triggered')
 			handleGesure('thirdQuestionContainer','teacherPageContainer','successPageContainer')
 			if(swipedLeft){
-				$('body').css({'background':'linear-gradient(270deg, #f4ee19, #1818ed)'})
+				$('body').css({'background':'linear-gradient(270deg, #1818ed, #f4ee19)','background-size':'400% 400%'})
 				swipedLeft = false
 			} else if(swipedRight){
-				$('body').css({'background':'linear-gradient(270deg, #1818ed, #12002b)'})
+				$('body').css({'background':'linear-gradient(270deg, #1818ed, #12002b)','background-size':'400% 400%'})
 				swipedRight = false
 			}
 		} else if (currentPage == 'successPageContainer'){
