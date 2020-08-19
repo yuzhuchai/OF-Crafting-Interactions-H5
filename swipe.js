1b1d602d83294b2b2f1a50b31c9488f49702079a
新媒体01 <designer01@shejishi01deiMac.local>
2020-08-19 19:49:33 +0800

    charging animation finished

--- swipe.js
+++ swipe.js
@@ -35,14 +35,22 @@ $( document ).ready(function() {
 		if(currentPageDisplay == 'homePageContainer'){
 			handleGesure('homePageContainer','teacherPageContainer','firstQuestionContainer')
 			// console.log(currentPage,'<inside event listener')
-		} else if(currentPageDisplay == 'teacherPageContainer' ||currentPageDisplay == 'teacherPageContainerFromQ3' || currentPageDisplay =='teacherPageContainerFromQ2'){
+		} else if(currentPageDisplay == 'teacherPageContainer' || currentPageDisplay == 'teacherPageContainerFromQ3' || currentPageDisplay =='teacherPageContainerFromQ2'){
+			$('body').css('background-color', '#1818ed');
+			console.log('this should be triggered')
 			handleGesure(currentPageDisplay, 'firstQuestionContainer', currentPageDisplay)
 		} else if(currentPageDisplay == 'firstQuestionContainer'){
 			handleGesure('firstQuestionContainer','secondQuestionContainer','teacherPageContainer')
 		} else if (currentPageDisplay == 'secondQuestionContainer'){
+			$('body').css('background-color','#e8005f');
 			handleGesure('secondQuestionContainer','thirdQuestionContainer','teacherPageContainerFromQ2')
 		} else if (currentPageDisplay == 'thirdQuestionContainer'){
+			$('body').css('background-color','#14e236');
+			console.log('change bg color triggered')
 			handleGesure('thirdQuestionContainer','teacherPageContainerFromQ3','successPageContainer')
+		} else if (currentPage == 'successPageContainer'){
+			// $('body').css('background-color','yellow');
+			// console.log('body BCcolor greem')
 		}
 
 	},false);
